89f8f52e14e0a835f831a2f225be8c9855223a98
Julio Cesar <juliocesarnb99@gmail.com>
2025-10-02 10:53:02 -0300

    Update frontend final

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -4,3 +4,6 @@ import axios from 'axios';
 export const httpClient = axios.create({
   baseURL: 'https://at4hm6e5ab.execute-api.us-east-1.amazonaws.com', // ← URL corrigida
 });
+/* export const httpClient = axios.create({
+  baseURL: 'https://rq5oob4i9k.execute-api.us-east-1.amazonaws.com',
+}); localhost:4571/*/
